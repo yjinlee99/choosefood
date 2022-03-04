6841ddaaa39885a02af70c1ed247adab976664ed
Yeong Jin <yjinlee99@naver.com>
2022-03-05 01:04:32 +0900

    spring react 연동

---
+++ back/food-project/src/main/java/food/foodproject/FoodProjectApplication.java
@@ -0,0 +1,14 @@
+package food.foodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodProjectApplication.class, args);
+	}
+
+
+}
