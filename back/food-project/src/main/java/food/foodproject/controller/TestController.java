ced49f2a1497466f2137a7e1e5680710aa51715a
insun <mhestim826@naver.com>
2022-05-19 19:39:09 +0900

    insun

--- back/food-project/src/main/java/food/foodproject/controller/TestController.java
+++ back/food-project/src/main/java/food/foodproject/controller/TestController.java
@@ -3,9 +3,7 @@ package food.foodproject.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,5 +36,13 @@ public class TestController {
     @GetMapping("/dtos")
     public ArrayList<DTO> getDtos(){
         return dtos;
+    }
+
+    // application/json 으로 받을 때
+    @PostMapping(value = "/test")
+    public String test(@RequestBody String str) {
+        System.out.println(str);
+
+        return "testIndex";
     }
 }
