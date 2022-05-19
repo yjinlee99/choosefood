7bdb5c26fd946d7142f1e1b25f42b67adecce8b5
Yeong Jin <yjinlee99@naver.com>
2022-05-19 23:18:06 +0900

    merge

--- front/foodchoose/src/Recipe.js
+++ front/foodchoose/src/Recipe.js
@@ -4,213 +4,125 @@ import SearchIcon from '@material-ui/icons/Search';
 import StarIcon from '@material-ui/icons/StarRounded';
 import Banner from './Banner';
 import Tab from './Tab';
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom';
 
 function Recipe() {
-    const [dtos, setDtos] = useState([[]]);
-    useEffect(() => {
-        fetch('/dtos')
-            .then(response => response.json())
-            .then(dtos => {
-                setDtos(dtos);
-            });
-    },[])
-    return (
-        <div className="Recipe">
-            
-            <div className="Search">
-                <input className="Search-input" type="text" />
-                <Link to="/SearchedRecipe"><SearchIcon className="Search-icon"/></Link>
-            </div>
+  const [dtos, setDtos] = useState([[]]);
+  useEffect(() => {
+    fetch('/dtos')
+      .then((response) => response.json())
+      .then((dtos) => {
+        setDtos(dtos);
+      });
+  }, []);
 
-            <div className="Header">
-                <Banner/>
-                <Tab />
-            </div>
+  let Info = [
+    {
+      id: 1,
+      thump: '썸',
+      profile: '프',
+      title: '타',
+      star: <StarIcon />,
+      hit: '조',
+    },
+    {
+      id: 2,
+      thump: '네',
+      profile: '로',
+      title: '이',
+      star: <StarIcon />,
+      hit: '회',
+    },
+    {
+      id: 3,
+      thump: '일',
+      profile: '필',
+      title: '틀',
+      star: <StarIcon />,
+      hit: '수',
+    },
+    {
+      id: 4,
+      thump: '썸',
+      profile: '프',
+      title: '타',
+      star: <StarIcon />,
+      hit: '조',
+    },
+    {
+      id: 5,
+      thump: '네',
+      profile: '로',
+      title: '이',
+      star: <StarIcon />,
+      hit: '회',
+    },
+    {
+      id: 6,
+      thump: '일',
+      profile: '필',
+      title: '틀',
+      star: <StarIcon />,
+      hit: '수',
+    },
+  ];
 
-            <select id="Array">
-                <option> 이름순 </option>
-                <option> 조회순 </option>
-                <option> 별점순 </option>
-            </select>
-            
-            <div id="Container">
-                <div id="List1" className="Content-list">
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src={dtos[0].thumbnail} width="290" height="160" />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">{(dtos[0].title)}</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="Recipe">
+      <div className="Header">
+        <Banner />
+        <Tab />
+      </div>
 
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src={dtos[1]?.thumbnail} width="290" height="160"  />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">{(dtos[1]?.title)}</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
+      <div className="Search">
+        <input className="Search-input" type="text" />
+        <Link to="/SearchedRecipe">
+          <SearchIcon className="Search-icon" />
+        </Link>
+      </div>
 
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src="" />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">제목</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
+      <select id="Array">
+        <option> 이름순 </option>
+        <option> 조회순 </option>
+        <option> 별점순 </option>
+      </select>
+
+      <div id="info-con">
+        <div id="Container">
+          {dtos.map((a) => (
+            <div className="Content">
+              <div className="Thump">
+                <Link to="/SingleRecipe">
+                  <div className="Thump-link">
+                    <img src={a.thumbnail} width="350" height="160" />
+                  </div>
+                </Link>
+              </div>
+
+              <div className="Detail">
+                <div className="Profile">
+                  <Link to="">
+                    <div className="Profile-link"> {a.profile}</div>
+                  </Link>
                 </div>
 
-                <div id="List2" className="Content-list">
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src="" />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">제목</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
+                <h4 className="Title">
+                  <Link to="">
+                    <div className="Title-link">{a.title}</div>
+                  </Link>
+                </h4>
 
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src="" />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">제목</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="Content">
-                        <Link to="/SingleRecipe">
-                        <div className="Thump">
-                                <img src="" />썸네일
-                        </div>
-                        </Link>
-                        <div className="Detail">
-                            <div className="Profile">
-                                <Link to="">
-                                    <img src=""/>프로필
-                                </Link>
-                            </div>
-                            <h4 className="Title">
-                                <Link to=""><div className="Title-link">제목</div></Link>
-                            </h4>
-                            <div>
-                                <div className="Stars">
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                    <StarIcon className="Star-icon"/>
-                                </div> &nbsp;
-                                <div className="Hits">조회수</div>
-                            </div>
-                        </div>
-                    </div>
+                <div>
+                  <div className="Stars"> {a.star} </div> &nbsp;
+                  <div className="Hits"> {a.hit} </div>
                 </div>
+              </div>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
 
 export default Recipe;
